32e1b1f65c355d930bfb7b99137137f80323e607
juea01 <akashwarkalay@gmail.com>
2017-04-28 11:24:20 +1200

    initial commit

---
+++ src/com/eworldtrade/imageservice/ImageServlet.java
@@ -0,0 +1,78 @@
+package com.eworldtrade.imageservice;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ImageServlet
+ */
+@WebServlet("/ImageServlet/*")
+public class ImageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String imagePath;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ImageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+        
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		imagePath = "/Users/macbookair/Documents/uploadFiles/";
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String requestedImage = request.getPathInfo();
+		
+		if (requestedImage == null) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+		
+		
+		File image = new File(imagePath, URLDecoder.decode(requestedImage,"UTF-8"));
+		
+		if (!image.exists()) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+		
+		
+		String contentType = getServletContext().getMimeType(image.getName());
+		
+		if (contentType == null || !contentType.startsWith("image")) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+		
+		response.reset();
+		response.setContentType(contentType);
+		response.setHeader("Content-Length", String.valueOf(image.length()));
+		
+		Files.copy(image.toPath(), response.getOutputStream());								
+		
+		
+	}
+
+}
